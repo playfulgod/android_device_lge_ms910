433f5b605f4b54d4288c22d3532b29ac29a16663
playfulgod <playfulgod@gmail.com>
2011-12-02 13:18:02 -0500

    added graphics.c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,19 +25,21 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+#BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+#BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
 
-BOARD_HAS_NO_MISC_PARTITON := true
+#BOARD_HAS_NO_MISC_PARTITON := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 USE_EXT4 := true
 BOARD_HAS_SDCARD_INTERNAL := true
 
 # custom lun0 file for USB Mass Storage
-BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun2/file
 
 TARGET_PREBUILT_KERNEL := device/lge/esteem/kernel
+
+BOARD_CUSTOM_GRAPHICS := ../../../device/lge/esteem/recovery/graphics.c
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
