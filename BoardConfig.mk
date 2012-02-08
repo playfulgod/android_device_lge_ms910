08e93d052f4a635c3f1cc66c1df62ffb63495734
playfulgod <playfulgod@gmail.com>
2012-02-08 18:58:02 -0500

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,9 +3,6 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/lge/esteem/BoardConfigVendor.mk
 
-# inherit from common msm7x30 device
--include device/htc/msm7x30-common/BoardConfigCommon.mk
-
 TARGET_NO_BOOTLOADER := true
 
 ## ARMv7 Processor rev 2 (v7l)
@@ -16,16 +13,17 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_BOOTLOADER_BOARD_NAME := esteem
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-
 BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=bryce
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_PAGE_SIZE := 0x00000800
 
 ## Partition sizes
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00800000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00800000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_HAS_NO_MISC_PARTITION := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1031798784
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1610612736
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
@@ -35,10 +33,8 @@ BOARD_HAS_NO_MISC_PARTITON := true
 
 BOARD_HAS_SDCARD_INTERNAL := true
 
-# custom lun0 file for USB Mass Storage
-BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun2
-BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun2/file
+TARGET_PROVIDES_LIBAUDIO := false
+BOARD_USES_ALSA_AUDIO := true
 
 TARGET_PREBUILT_KERNEL := device/lge/esteem/kernel
 
--- esteem.mk
+++ esteem.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/lge/esteem/esteem-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/esteem/device-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/esteem/overlay
 
@@ -22,9 +22,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
-
-# common msm7x30 configs
-$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_esteem
