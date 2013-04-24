aa2d6aa823106e804fe912e33bd0af54d775faeb
PlayfulGod <playfulgod@gmail.com>
2013-04-24 07:00:34 -0400

    edit remove libaudio.so and libv8.so from vendor blobs

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,8 +26,6 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 PRODUCT_COPY_FILES := \\
     $OUTDIR/proprietary/lib/liboncrpc.so:obj/lib/liboncrpc.so \\
     $OUTDIR/proprietary/lib/libnv.so:obj/lib/libnv.so \\
-    $OUTDIR/proprietary/lib/libaudio.so:obj/lib/libaudio.so \\
-    $OUTDIR/proprietary/lib/libv8.so:obj/lib/libv8.so
 
 PRODUCT_COPY_FILES += \\
 EOF
